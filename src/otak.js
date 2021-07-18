d3397ce0da34d52aa93a1417e0a9064c798caccf
Beimarsempai <87555802+Beimarsempai@users.noreply.github.com>
2021-07-17 20:29:50 -0400

    Add files via upload

---
+++ src/otak.js
@@ -0,0 +1,27 @@
+const otak = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
+
+
+bañate
+pack
+gime
+quien es tu sempai?
+yamete
+buenas
+a
+sexual
+te amo
+konede
+ara ara
+bemos netflix?
+gimeme
+onichan
+senpai
+
+_Ojito escribe tal como esta_
+
+ву Beimar`
+
+}
+
+exports.otak = otak
